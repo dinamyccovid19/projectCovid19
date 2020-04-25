e171d18147dc726e06d22d1b84faa0bfaf0e8b6f
german <germantagli@gmail.com>
2020-04-25 19:35:20 +0200

    creazione delle nuove pagine

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
+
+
+
 const routes: Routes = [
   {
     path: 'tabs',
@@ -18,6 +21,18 @@ const routes: Routes = [
       {
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      },
+      {
+        path: 'tab4',
+        loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule)
+      },
+      {
+        path: 'tab5',
+        loadChildren: () => import('../tab5/tab5.module').then(m => m.Tab5PageModule)
+      },
+      {
+        path: 'tab6',
+        loadChildren: () => import('../tab6/tab6.module').then(m => m.Tab6PageModule)
       },
       {
         path: '',
