9b25bf60530ecb144711950b76a56941579e863d
german <germantagli@gmail.com>
2020-04-25 20:32:55 +0200

    qr code

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -6,6 +6,7 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { QRScanner } from '@ionic-native/qr-scanner/ngx';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
+  providers: [QRScanner],
   declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
