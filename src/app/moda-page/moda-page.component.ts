5046064d9f3c0ac222fb158adbce30d32ca9f493
german <germantagli@gmail.com>
2020-04-26 11:36:40 +0200

    fix form

---
+++ src/app/moda-page/moda-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moda-page',
+  templateUrl: './moda-page.component.html',
+  styleUrls: ['./moda-page.component.scss'],
+})
+export class ModaPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
