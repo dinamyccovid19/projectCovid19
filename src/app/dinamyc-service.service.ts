a4d53148de4463f7197f27f2ecb256f58b42ea8d
german <germantagli@gmail.com>
2020-04-26 18:00:20 +0200

    Fix notification

--- src/app/dinamyc-service.service.ts
+++ src/app/dinamyc-service.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DinamycServiceService {
 
-  constructor(public loadingController: LoadingController ) { }
+  constructor(public loadingController: LoadingController, public toastController: ToastController ) { }
   async presentLoading() {
     const loading = await this.loadingController.create({
       message: 'Please wait...',
@@ -27,5 +28,15 @@ export class DinamycServiceService {
 
     const { role, data } = await loading.onDidDismiss();
     console.log('Loading dismissed!');
+    this.presentToast();
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      color:"success",
+      message: 'Gentle user operation is confirmed',
+      duration: 2000
+    });
+    toast.present();
   }
 }
--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -10,18 +10,38 @@ import { ModaPageComponent } from '../moda-page/moda-page.component';
 })
 export class Tab5Page {
 
+  roomList=[
+    {
+      "address":"Via del vespro, 129 - Palermo",
+      "name":"Laboratorio Microcopica Electronica",
+      "description":"  Keep close to Natures heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.",
+      "isPrenotabile":true,
+      "numero_postiDisponibile":5
+    },
+    {
+      "address":"Vai del Castro Petrorio - Roma",
+      "name":"Laboratorio",
+      "description":"  Keep close to Natures heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.",
+      "isPrenotabile":false,
+      "numero_postiDisponibile":0
+    },
+    {
+      "address":"Via del Castro Petrorio - Roma",
+      "name":"Laboratorio Tecnologia e Informatica",
+      "description":"  Keep close to Natures heart... and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean.",
+      "isPrenotabile":true,
+      "numero_postiDisponibile":5
+    }
+  ]
   constructor(private service:DinamycServiceService, public modalController: ModalController) {}
 
   ionViewDidEnter(){
     this.service.presentLoading();
   }
 
-  async presentModal() {
-    alert("popup")
-    const modal = await this.modalController.create({
-      component: ModaPageComponent
-    });
-    return await modal.present();
+  async presentModal(item) {
+    item.numero_postiDisponibile = item.numero_postiDisponibile - 1;
+    this.service.presentToast();
   }
 
 }
